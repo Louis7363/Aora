d91955e8795292030bfda9dee03f0d4fcfc07407
Louis7363 <louiscrt@sfr.fr>
2024-10-31 15:54:21 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora !</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color: 'blue'}}>Go to profile</Link>
+    </View>
+  );
+}
